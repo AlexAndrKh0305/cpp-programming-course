61a65eb723e402870495e1589b314d1f8facbaf9
Alexandr Khrenov <90575769+AlexAndrKh0305@users.noreply.github.com>
2023-09-14 12:46:55 +0300

    MyFirstProg

---
+++ MyFirstProg.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	std::cout << "Hello, World!\n";
+	std::cout << "Привет, Мир!" << std::endl;
+	return 0;
+}
