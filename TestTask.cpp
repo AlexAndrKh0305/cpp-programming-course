5bb9ebbf1f924ce2b76e4b58f2629a98bd1a0000
Alexandr Khrenov <90575769+AlexAndrKh0305@users.noreply.github.com>
2023-09-15 17:49:05 +0300

    Edit comments

--- TestTask.cpp
+++ TestTask.cpp
@@ -10,24 +10,24 @@ int main() {
 	double y1, y2, y3, y4, y5;
 
 	std::cout << "Введите координаты первой точки: ";
-	std::cin >> x1 >> y1; // 2 -2
+	std::cin >> x1 >> y1;
 
 	std::cout << "Введите координаты второй точки: ";
-	std::cin >> x2 >> y2; // 2 5
+	std::cin >> x2 >> y2;
 
 	std::cout << "Введите координаты третьей точки: ";
-	std::cin >> x3 >> y3; // 5 9
+	std::cin >> x3 >> y3;
 
 	std::cout << "Введите координаты четвертой точки: ";
-	std::cin >> x4 >> y4; // 7 5
+	std::cin >> x4 >> y4;
 
 	std::cout << "Введите координаты пятой точки: ";
-	std::cin >> x5 >> y5; // 7 -2
+	std::cin >> x5 >> y5;
 
 	double square = std::abs(x1 * y2 + x2 * y3 + x3 * y4 + x4 * y5 + x5 * y1
 	- x2 * y1 - x3 * y2 - x4 * y3 - x5 * y4 - x1 * y5) / 2; // Формула площади Гаусса
 
-	std::cout << square; // 45
+	std::cout << square;
 	return 0;
 }
 
