803061b226bd23cdf13697207a2be0c6af109f3e
Alexandr Khrenov <90575769+AlexAndrKh0305@users.noreply.github.com>
2023-10-21 15:13:15 +0300

    14 lesson

---
+++ TestTask1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+template<class T>
+T average(T arr[], int size) {
+	T sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += arr[i];
+	}
+	return (sum / size);
+}
+
+int main() {
+
+	long arr1[] = {100, 99, 80};
+	double arr2[] = {3.7, 2.9, 23.8, 27.1};
+	char arr3[] = {'a', 'b', 'c', 'd', 'e'};
+
+	int k1 = sizeof(arr1) / sizeof(arr1[0]);
+	int k2 = sizeof(arr2) / sizeof(arr2[0]);
+	int k3 = sizeof(arr3) / sizeof(arr3[0]);
+
+	std::cout << average(arr1, k1) << "\n";
+	std::cout << average(arr2, k2) << "\n";
+	std::cout << average(arr3, k3) << "\n";
+
+
+	return 0;
+}
---
+++ TestTask2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+
+template<class Container>
+void Print(const Container& cont, std::string str) {
+
+	for (auto it{ cont.begin() }; it != cont.end(); it++)
+	{
+		std::cout << *it;
+		if (it + 1 != cont.end()) {
+			std::cout << str;
+		}
+	}
+	std::cout << std::endl;
+
+}
+
+int main() {
+	std::vector<int> data = { 1, 2, 3 };
+	std::array<char, 4> arr = { 'a', 'b', 'c', 'd' };
+	Print(data, ", ");
+	Print(arr, "-+-");
+	return 0;
+}
