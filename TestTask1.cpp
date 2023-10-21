628be45e26b4da0fc86087b296d079a4a18973a2
Alexandr Khrenov <90575769+AlexAndrKh0305@users.noreply.github.com>
2023-10-21 15:14:00 +0300

    15 lesson

---
+++ TestTask1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <map>
+
+struct StudentGrade {
+	std::string name;
+	char grade;
+};
+
+int main() {
+
+	StudentGrade studentGrade1 = { "Alex", 'A' };
+	StudentGrade studentGrade2 = { "Ben", 'B' };
+	StudentGrade studentGrade3 = { "Alex", 'C' };
+
+	std::map<std::string, char> grades;
+
+	grades[studentGrade1.name] = studentGrade1.grade;
+	grades[studentGrade2.name] = studentGrade2.grade;
+	grades[studentGrade3.name] = studentGrade3.grade;
+
+	for (auto grade : grades) {
+		std::cout << grade.first << " " << grade.second << "\n";
+	}
+	return 0;
+}
