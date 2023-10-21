4ed0af74a53c480bb892d41c224dfde0e03aa8f3
Alexandr Khrenov <90575769+AlexAndrKh0305@users.noreply.github.com>
2023-10-21 15:08:05 +0300

    9 lesson

---
+++ TestTask1.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+
+class Triangle {
+
+public:
+
+	class NegativeSideError {
+
+	public:
+
+		NegativeSideError() {
+			message = "Triangle can't include negative side!";
+		}
+
+		void printMessage() {
+			std::cout << message << "\n";
+		}
+
+	private:
+
+		std::string message;
+	};
+
+	class TooBigSideError {
+
+	public:
+
+		TooBigSideError() {
+			message = "One of the sides is greater or equal than sum of other!";
+		}
+
+		void printMessage() {
+			std::cout << message << "\n";
+		}
+
+	private:
+
+		std::string message;
+	};
+
+	Triangle(double a, double b, double c) {
+		if (a < 0 || b < 0 || c < 0) {
+			throw NegativeSideError();
+		} else if (a + b <= c || a + c <= b || b + c <= a) {
+			throw TooBigSideError();
+		}
+		side1 = a;
+		side2 = b;
+		side3 = c;
+	}
+
+	double square() {
+		double semiPerimeter = (side1 + side2 + side3) / 2;
+		return std::sqrt(semiPerimeter * (semiPerimeter - side1)
+			* (semiPerimeter - side2) * (semiPerimeter - side3));
+	}
+
+private:
+	double side1;
+	double side2;
+	double side3;
+
+};
+
+int main() {
+
+	double side1, side2, side3;
+
+	std::cout << "side1 = ";
+	std::cin >> side1;
+	std::cout << "side2 = ";
+	std::cin >> side2;
+	std::cout << "side3 = ";
+	std::cin >> side3;
+
+	try {
+		Triangle* triangle = new Triangle(side1, side2, side3);
+		std::cout << triangle->square();
+	} catch (Triangle::NegativeSideError& error) {
+		error.printMessage();
+	} catch (Triangle::TooBigSideError& error) {
+		error.printMessage();
+	}
+
+	return 0;
+}
---
+++ TestTask2.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+
+class Time {
+
+public:
+
+	class NegativeNumberError {
+
+	public:
+		NegativeNumberError() {
+			message = "One of the numbers is negative";
+		}
+
+		void printMessage() {
+			std::cout << message << "\n";
+		}
+
+	private:
+		std::string message;
+
+	};
+
+	Time() {
+		hours = 0;
+		minutes = 0;
+		seconds = 0;
+	}
+
+	Time(int h, int m, int s) {
+
+		if (h < 0 || m < 0 || s < 0) {
+			throw NegativeNumberError();
+		}
+
+		seconds = s % 60;
+
+		m += s / 60;
+		minutes = m % 60;
+
+		h += m / 60;
+		hours = h;
+	}
+
+	void showTime() const {
+		int h = hours % 12;
+		int m = minutes;
+		int s = seconds;
+
+		std::cout << "\n";
+		if (h < 10) {
+			std::cout << 0;
+		}
+		std::cout << h << ":";
+		if (m < 10) {
+			std::cout << 0;
+		}
+		std::cout << m << ":";
+		if (s < 10) {
+			std::cout << 0;
+		}
+		std::cout << s << "\n";
+	}
+
+	Time* addTime(Time* time) const {
+		int s = seconds + (*time).seconds;
+
+		int m = minutes + (*time).minutes + s / 60;
+		s %= 60;
+
+		int h = hours + (*time).hours + m / 60;
+		m %= 60;
+
+		Time* sumTime = new Time(h, m, s);
+
+		return sumTime;
+	}
+
+private:
+	int hours;
+	int minutes;
+	int seconds;
+
+};
+
+int main() {
+	int h1, m1, s1;
+
+	std::cout << "h1 = ";
+	std::cin >> h1;
+	std::cout << "m1 = ";
+	std::cin >> m1;
+	std::cout << "s1 = ";
+	std::cin >> s1;
+
+	int h2, m2, s2;
+
+	std::cout << "h2 = ";
+	std::cin >> h2;
+	std::cout << "m2 = ";
+	std::cin >> m2;
+	std::cout << "s2 = ";
+	std::cin >> s2;
+
+	try {
+		Time* time1 = new Time(h1, m1, s1);
+
+		Time* time2 = new Time(h2, m2, s2);
+
+		Time* time3 = time1->addTime(time2);
+
+		time3->showTime();
+	} catch (Time::NegativeNumberError& error) {
+		error.printMessage();
+	}
+	return 0;
+}
